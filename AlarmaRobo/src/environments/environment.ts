238b24f7ab1f2d92d408152ebeb78b746cbae443
Brian Pariz <brianpariz@hotmail.com>
2020-07-08 10:31:35 -0400

    Update environment.ts

--- AlarmaRobo/src/environments/environment.ts
+++ AlarmaRobo/src/environments/environment.ts
@@ -5,13 +5,7 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: "AIzaSyDw9dADzz_0MKb-eG6hnF7CEZPRxWKSSzI",
-    authDomain: "practica-profesional-79e57.firebaseapp.com",
-    databaseURL: "https://practica-profesional-79e57.firebaseio.com",
-    projectId: "practica-profesional-79e57",
-    storageBucket: "practica-profesional-79e57.appspot.com",
-    messagingSenderId: "638479227465",
-    appId: "1:638479227465:web:e6e0f2f6940c51cc1cfd98"
+
   }
 };
 
